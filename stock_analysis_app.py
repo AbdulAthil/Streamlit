bd732f3950d32111587a59de6b06c7b78a12ad2a
Abdul Athil <139438718+AbdulAthil@users.noreply.github.com>
2023-11-29 16:40:04 +0530

    Update stock_analysis_app.py

--- stock_analysis_app.py
+++ stock_analysis_app.py
@@ -84,18 +84,20 @@ with tab12:
         # st.write("Selected dataset: ", compare_stocks1)
         if compare_stocks1 == "Select":
             st.write("Select a dataset from above")
+        button = st.button("Compare")
         st.divider()
-        st.write(f"##### {compare_stocks1} stock values")
-        # define a ticker symbol
-        ticker_symbol1 = compare_stocks1
-        # get data on this ticker
-        ticker_data1 = yf.Ticker(ticker_symbol1)
-        # get the historical prices for this ticker
-        START = "2023-1-1"
-        END = date.today().strftime("%Y-%m-%d")
-        tickerDf1 = ticker_data1.history(period="id", start=START, end=END)
-        st.dataframe(tickerDf1.tail())
-        st.divider()
+        if button:
+            st.write(f"##### {compare_stocks1} stock values")
+            # define a ticker symbol
+            ticker_symbol1 = compare_stocks1
+            # get data on this ticker
+            ticker_data1 = yf.Ticker(ticker_symbol1)
+            # get the historical prices for this ticker
+            START = "2023-1-1"
+            END = date.today().strftime("%Y-%m-%d")
+            tickerDf1 = ticker_data1.history(period="id", start=START, end=END)
+            st.dataframe(tickerDf1.tail())
+            st.divider()
 
     # Second company
     with col2:
@@ -111,66 +113,68 @@ with tab12:
         if compare_stocks2 == "Select":
             st.write("Select a dataset from above")
         st.divider()
-        st.write(f"##### {compare_stocks2} stock values")
-        # define a ticker symbol
-        ticker_symbol2 = compare_stocks2
-        # get data on this ticker
-        ticker_data2 = yf.Ticker(ticker_symbol2)
-        # get the historical prices for this ticker
-        START = "2023-1-1"
-        END = date.today().strftime("%Y-%m-%d")
-        tickerDf2 = ticker_data2.history(period="id", start=START, end=END)
-        st.dataframe(tickerDf2.tail())
-        st.divider()
+        if button:
+            st.write(f"##### {compare_stocks2} stock values")
+            # define a ticker symbol
+            ticker_symbol2 = compare_stocks2
+            # get data on this ticker
+            ticker_data2 = yf.Ticker(ticker_symbol2)
+            # get the historical prices for this ticker
+            START = "2023-1-1"
+            END = date.today().strftime("%Y-%m-%d")
+            tickerDf2 = ticker_data2.history(period="id", start=START, end=END)
+            st.dataframe(tickerDf2.tail())
+            st.divider()
 
     # Visualization
     compare_companies = [compare_stocks1, compare_stocks2]
     cols = tickerDf1.columns
     # st.write(cols)
-    select_col = st.selectbox("Select which column you want to compare", cols)
-
-    tab1, tab2, tab3, tab4 = st.tabs(["Sum", "Mean", "Max", "Min"])
-    with tab1:
-        # Sum value
-        c1_sum = tickerDf1[select_col].sum()
-        st.write(compare_stocks1, "-", c1_sum)
-        c2_sum = tickerDf2[select_col].sum()
-        st.write(compare_stocks2, "-", c2_sum)
-        compare_sum = [c1_sum, c2_sum]
-        st.caption("Comparison")
-        df = pd.DataFrame(compare_sum, index=compare_companies)
-        st.bar_chart(df, use_container_width=True)
-
-    with tab2:
-        # Mean value
-        c1_mean = tickerDf1[select_col].mean()
-        st.write(compare_stocks1, "-", c1_mean)
-        c2_mean = tickerDf2[select_col].mean()
-        st.write(compare_stocks2, "-", c2_mean)
-        compare_mean = [c1_mean, c2_mean]
-        st.caption("Comparison")
-        df = pd.DataFrame(compare_mean, index=compare_companies)
-        st.bar_chart(df, use_container_width=True)
-
-    with tab3:
-        # Sum value
-        c1_max = tickerDf1[select_col].max()
-        st.write(compare_stocks1, "-", c1_max)
-        c2_max = tickerDf2[select_col].max()
-        st.write(compare_stocks2, "-", c2_max)
-        compare_max = [c1_max, c2_max]
-        st.caption("Comparison")
-        df = pd.DataFrame(compare_max, index=compare_companies)
-        st.bar_chart(df, use_container_width=True)
-
-    with tab4:
-        # Sum value
-        c1_min = tickerDf1[select_col].min()
-        st.write(compare_stocks1, "-", c1_min)
-        c2_min = tickerDf2[select_col].min()
-        st.write(compare_stocks2, "-", c2_min)
-        compare_min = [c1_min, c2_min]
-        st.caption("Comparison")
-        df = pd.DataFrame(compare_min, index=compare_companies)
-        st.bar_chart(df, use_container_width=True)
-
+    if button:
+        select_col = st.selectbox("Select which column you want to compare", cols)
+    
+        tab1, tab2, tab3, tab4 = st.tabs(["Sum", "Mean", "Max", "Min"])
+        with tab1:
+            # Sum value
+            c1_sum = tickerDf1[select_col].sum()
+            st.write(compare_stocks1, "-", c1_sum)
+            c2_sum = tickerDf2[select_col].sum()
+            st.write(compare_stocks2, "-", c2_sum)
+            compare_sum = [c1_sum, c2_sum]
+            st.caption("Comparison")
+            df = pd.DataFrame(compare_sum, index=compare_companies)
+            st.bar_chart(df, use_container_width=True)
+    
+        with tab2:
+            # Mean value
+            c1_mean = tickerDf1[select_col].mean()
+            st.write(compare_stocks1, "-", c1_mean)
+            c2_mean = tickerDf2[select_col].mean()
+            st.write(compare_stocks2, "-", c2_mean)
+            compare_mean = [c1_mean, c2_mean]
+            st.caption("Comparison")
+            df = pd.DataFrame(compare_mean, index=compare_companies)
+            st.bar_chart(df, use_container_width=True)
+    
+        with tab3:
+            # Sum value
+            c1_max = tickerDf1[select_col].max()
+            st.write(compare_stocks1, "-", c1_max)
+            c2_max = tickerDf2[select_col].max()
+            st.write(compare_stocks2, "-", c2_max)
+            compare_max = [c1_max, c2_max]
+            st.caption("Comparison")
+            df = pd.DataFrame(compare_max, index=compare_companies)
+            st.bar_chart(df, use_container_width=True)
+    
+        with tab4:
+            # Sum value
+            c1_min = tickerDf1[select_col].min()
+            st.write(compare_stocks1, "-", c1_min)
+            c2_min = tickerDf2[select_col].min()
+            st.write(compare_stocks2, "-", c2_min)
+            compare_min = [c1_min, c2_min]
+            st.caption("Comparison")
+            df = pd.DataFrame(compare_min, index=compare_companies)
+            st.bar_chart(df, use_container_width=True)
+    
