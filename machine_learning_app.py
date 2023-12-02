85dd51c1df5e2fce57d25f22ad2ce634c6621eb8
Abdul Athil <139438718+AbdulAthil@users.noreply.github.com>
2023-12-02 13:00:33 +0530

    Update machine_learning_app.py

--- machine_learning_app.py
+++ machine_learning_app.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn import metrics
 from sklearn.preprocessing import LabelEncoder
 
