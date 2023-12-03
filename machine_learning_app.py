f593d62dee0dedf426cc9e8197a6bc2402ebc8b8
Abdul Athil <139438718+AbdulAthil@users.noreply.github.com>
2023-12-03 22:26:11 +0530

    Update machine_learning_app.py

--- machine_learning_app.py
+++ machine_learning_app.py
@@ -6,140 +6,140 @@ from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn import metrics
 from sklearn.preprocessing import LabelEncoder
 
-# Page layout - Page expands to full width
-st.set_page_config(page_title='Machine Learning App', layout='wide')
-# Model building
+# Page layout - Page is centered
+st.set_page_config(page_title='Machine Learning App', layout='centered')
+
+# Function to build and evaluate the machine learning model
 
 
-def build_model(df):
-    st.write("#### Select Target column")
-    target_column = st.text_input("Select your Target column which you want to analyse")
+def build_and_evaluate_model(df, target_column, missing_value_strategy):
+    st.write("### Machine Learning Model Evaluation")
 
-    if target_column:
-        st.write(f"You selected {target_column} as target_column")
-        x = df.drop(columns=[target_column])  # Using all column except target_column for as X
-        y = df[target_column]  # Using the target_column as Y
-    else:
-        st.write("You did not select any specific target_column. "
-                 "So the model will take the last column as target column")
-        x = df.iloc[:, :-1]  # Using all column except for the last column as X
-        y = df.iloc[:, -1]  # Selecting the last column as Y
+    # Handling missing values
+    if missing_value_strategy == "Drop Rows with Missing Values":
+        df = df.dropna()
+    elif missing_value_strategy == "Fill with Mean":
+        df = df.fillna(df.mean())
+    elif missing_value_strategy == "Fill with Median":
+        df = df.fillna(df.median())
+    elif missing_value_strategy == "Fill with Zero":
+        df = df.fillna(0)
+    st.write(df.head())
+
+    # Data preprocessing
+    x = df.drop(columns=[target_column])
+    y = df[target_column]
 
     # Data splitting
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=(100 - split_size) / 100,
                                                         random_state=parameter_random_state)
 
+    # Variable details
     st.write("#### Variable details")
-    st.write("**x - variables(Independent)**")
-    st.write(list(x.columns))
-    st.write("**y - variable(Dependent)**")
-    st.write(y.name)
+    st.write("**Features (X):**")
+    st.write(f"***{list(x.columns)}***")
+    st.write("**Target variable (Y):**")
+    st.write(f"***{target_column}***")
     st.divider()
 
+    # Data splits
     st.write("#### Data splits")
-    st.write("* **Training set**", x_train.shape)
-    st.write("* **Test set**", x_test.shape)
+    st.write("* **Training set:**", x_train.shape)
+    st.write("* **Test set:**", x_test.shape)
     st.divider()
 
     # Model Building
-    rf = RandomForestClassifier(n_estimators=parameter_n_estimators,
-                                random_state=parameter_random_state)
-    rf.fit(x_train, y_train)
+    st.subheader("Model Building")
 
-     # Prediction
-    y_predict_test = rf.predict(x_test)
+    # Check if the dtype is numeric (continuous) or discrete
+    if np.issubdtype(y.dtype, np.number):  
+        st.info("Target variable is ***continuous***. Applying ***RandomForestRegressor***.")
+        st.divider()
+        model = RandomForestRegressor(n_estimators=parameter_n_estimators, random_state=parameter_random_state)
+    else:
+        st.info("Target variable is ***discrete***. Applying ***RandomForestClassifier***.")
+        model = RandomForestClassifier(n_estimators=parameter_n_estimators, random_state=parameter_random_state)
+
+    # Checking the target variable distribution
+    if len(y.unique()) < 2:
+        st.warning("Target variable has less than 2 unique values. Check your data.")
+
+    # Fit the train data into our model
+    model.fit(x_train, y_train)
+
+    # Prediction
+    y_predict_test = model.predict(x_test)
 
     # Evaluation
-    st.write("#### Model Performance")
-    st.write("* ###### Classification Report")
-    report = metrics.classification_report(y_test, y_predict_test)
-    st.text(report)
-    st.write("")
-    st.write("* ###### Accuracy")
-    accuracy = rf.score(x_test, y_test)*100
-    st.info(f"Model Accuracy = ***{round(accuracy,3)}***")
-    st.write(" ")
-    st.write("* ###### Confusion Matrix")
-    matrix = metrics.confusion_matrix(y_test, y_predict_test)
-    st.table(matrix)
+    st.subheader("Model Performance")
 
-    st.write("* ###### Cross Validation scores")
+   
+    if np.issubdtype(y.dtype, np.number):  
+        # Regression metrics        
+        accuracy = model.score(x_test, y_test) * 100
+        st.info(f"**Model Accuracy** = ***{round(accuracy, 3)}***")
+        st.write("* ***Mean Absolute Error (MAE):***", round(metrics.mean_absolute_error(y_test, y_predict_test), 3))
+        st.write("* ***Mean Squared Error (MSE):***", round(metrics.mean_squared_error(y_test, y_predict_test), 3))
+        st.write("* ***R-squared:***", round(metrics.r2_score(y_test, y_predict_test), 3))
+    else:  
+        # Classification metrics
+        st.write("* **Classification Report:**")
+        st.text(metrics.classification_report(y_test, y_predict_test))
+        st.write("* **Accuracy:**", round(metrics.accuracy_score(y_test, y_predict_test), 3))
+        st.write("* **Confusion Matrix:**")
+        st.table(metrics.confusion_matrix(y_test, y_predict_test))
+
+    # Cross validation
+    st.write("##### Cross Validation scores")
     col1, col2 = st.columns(2)
     with col1:
-        cv_acc = cross_val_score(rf, x, y, cv=3, scoring="accuracy")
-        st.info(f"* Accuracy score = ***{round(np.mean(cv_acc),4)*100}***")
-        cv_precision = cross_val_score(rf, x, y, cv=3, scoring="precision_macro")
-        st.info(f"* Precision score = ***{round(np.mean(cv_precision),4)*100}***")
+        st.write("* **Accuracy score:**", round(np.mean(cross_val_score(model, x, y, cv=3, scoring="accuracy")), 4))
     with col2:
-        cv_recall = cross_val_score(rf, x, y, cv=3, scoring="recall_macro")
-        st.info(f"* Recall score = ***{round(np.mean(cv_recall),4)*100}***")
-        cv_f1 = cross_val_score(rf, x, y, cv=3, scoring="f1_macro")
-        st.info(f"* F1 score = ***{round(np.mean(cv_f1),4)*100}***")
+        if np.issubdtype(y.dtype, np.number):  # Regression metrics for cross-validation
+            st.write("* **R-squared:**", round(np.mean(cross_val_score(model, x, y, cv=3, scoring="r2")), 4))
+        else:  # Classification metrics for cross-validation
+            st.write("* **Precision score:**", round(np.mean(cross_val_score(model, x, y, cv=3, scoring="precision_weighted")), 4))
+
+    st.success("Model evaluation completed.")
     st.divider()
-
-
-# --------------------------------- #
-
-
-st.write("""# The Machine Learning App
-* By using this app, we can make *Machine Learning prediction* for any dataset.
-
-* In this app, ***RandomForestClassifier()*** function is used to build the model.
-
-* Upload your dataset in the sidebar or Click on the button shown below to run with the default dataset.
-""")
-st.divider()
-
-# --------------------------------- #
 
 # Sidebar
-with st.sidebar.header("Upload Your Data"):
-    # st.write("#### Upload your input CSV file")
-    uploaded_file = st.sidebar.file_uploader("*File must be in CSV format.*", type=["csv"])
 
-with st.sidebar.subheader("Set Parameters"):
-    split_size = st.sidebar.slider('**Data split ratio** (% for Training Set)', 10, 90, 75, 5)
-    parameter_n_estimators = st.sidebar.slider('Number of estimators (**n_estimators**)', 0, 1000, 100, 50)
-    parameter_random_state = st.sidebar.slider('Seed number (**random_state**)', 0, 1000, 42, 1)
 
-# --------------------------------- #
+st.sidebar.title("Machine Learning App")
+uploaded_file = st.sidebar.file_uploader("Upload your CSV file", type=["csv"])
+split_size = st.sidebar.slider('Data split ratio (% for Training Set)', 10, 90, 75, 5)
+parameter_n_estimators = st.sidebar.slider('Number of estimators (n_estimators)', 0, 1000, 100, 50)
+parameter_random_state = st.sidebar.slider('Seed number (random_state)', 0, 1000, 42, 1)
+missing_value_strategy = st.sidebar.selectbox("Missing Value Strategy", ["Drop Rows with Missing Values", "Fill with Mean", "Fill with Median", "Fill with Zero"])
+
 # Main panel
+st.title("Machine Learning App")
 
-# Displays the dataset
-
+# Upload file 
 if uploaded_file is not None:
-    df_user = pd.read_csv(uploaded_file)
-    df = df_user.dropna()
-    # st.write(df.isnull().sum())
-    st.write("#### Dataset Information")
+    df = pd.read_csv(uploaded_file)
+    st.subheader("Dataset Information")
     st.write(df.head())
-    st.write("* Dataset Shape :", df.shape)
-    cols = []
-    for columns in df.columns:
-        cols.append(columns)
-    st.write(f"* Columns : {cols}")
-    # st.write(df.describe().head(3))
+    st.write(f"* Dataset Shape : {df.shape}")
+    st.write(f"* Columns : {list(df.columns)}")
     st.divider()
+
+    # Label Encoding(convert categorical data)
     le = LabelEncoder()
     for column in df.columns:
         if df[column].dtype == "object":
             df[column] = le.fit_transform(df[column])
-    # st.write(df.head())
-    build_model(df)
+
+    # Target selection from user
+    st.write("### Target Column Selection")
+    st.write("If you don't select any specific column, "
+             "the *model* will take the first column as target column by default.")
+    target_column = st.selectbox("Select Target column", df.columns)
+    st.divider()
+    
+    build_and_evaluate_model(df, target_column, missing_value_strategy)
 
 else:
     st.info('Waiting for CSV file to be uploaded.')
-    button = st.checkbox('Click here to use Example Dataset')
-    if button:
-        df = pd.read_csv("diabetes.csv")
-
-        st.subheader("Dataset Information")
-        st.markdown('The Diabetes dataset is used as an example.')
-        st.write(df.head())
-        st.write("* Dataset Shape :", df.shape)
-        st.write(f"* Columns : {df.columns}")
-        # st.write(df.describe().head(3))
-
-        st.divider()
-
-        build_model(df)
+    st.warning('Please upload a CSV file to proceed.')
